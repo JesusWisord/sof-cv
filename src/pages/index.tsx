cb37870e4ae2bf4c45c32ea000d65dc00db72c09
Chuy <jduarte@nexu.mx>
2023-04-20 20:19:28 -0600

    fix: fixed styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.typekit.net/lyj6hpr.css" />
       </Head>
       <header className={styles.header}>
         <figure>
